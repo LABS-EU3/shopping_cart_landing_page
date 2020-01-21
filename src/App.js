0644274a2c7bbb770f6dc98cb4522bc91f6eb944
Shaun Orpen <shaunorpen@users.noreply.github.com>
2020-01-21 10:20:26 +0000

    Add HTML to home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import React from 'react'
 
 function App () {
-  return <div className='App'>Hello World!</div>
+  return (
+    <div>
+      <div>
+        <div>PureRetail</div>
+        <div>
+          <ul>
+            <li>How it works</li>
+            <li>Pricing</li>
+            <li>FAQ</li>
+            <li>Support</li>
+          </ul>
+        </div>
+        <div>
+          <button>Sign up</button>
+          <button>Log in</button>
+        </div>
+      </div>
+      <div>
+        <div>The easiest way to sell. Period.</div>
+        <div>
+          <ol>
+            <li>Set up your store in under ONE minute</li>
+            <li>Share your link with your customers</li>
+            <li>You're in business</li>
+          </ol>
+        </div>
+        <button>Sign Up Now</button>
+      </div>
+    </div>
+  )
 }
 
 export default App
