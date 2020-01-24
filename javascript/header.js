c14632e16c72cd04dfb19f37ad149f78a7dea421
mogwai <feelirabs@gmail.com>
2020-01-24 03:05:22 +0100

    Experimenting with new bg image

--- javascript/header.js
+++ javascript/header.js
@@ -1,17 +1,14 @@
-// When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
 
-// Get the header
-var header = document.getElementsByClassName('header');
+var navbar = document.getElementById('myHeader');
 
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
+var sticky = navbar.offsetTop;
 
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+
 function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
   } else {
-    header.classList.remove("sticky");
+    navbar.classList.remove("sticky");
   }
 }
